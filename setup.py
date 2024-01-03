209122dc4d8b96857ae092b46da4f02922480cb6
Sojib <20101030@uap-bd.edu>
2024-01-03 21:13:47 +0600

    Setup, Logger and CustomException

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_packages
+from typing import List
+
+
+hypen_e_dot="-e ."
+
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','')for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+
+    return requirements
+
+
+    
+setup(
+    name="Vechile_Type_Detection",
+    version='0.0.1',
+    author="sojib",
+    author_email="gsciansaimon@gmail.com",
+    packages=find_packages(),
+    install_requirements=get_requirements("requirements.txt")
+)
---
+++ src/VechileTypeDetection/logger.py
@@ -0,0 +1,41 @@
+import os
+import logging
+import sys
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+
+# Create 'logs' directory if it doesn't exist
+os.makedirs(log_dir, exist_ok=True)
+
+# Find the latest subdirectory number
+latest_subdir_number = 0
+for entry in os.listdir(log_dir):
+    if entry.startswith("running logs"):
+        try:
+            number = int(entry.split()[-1])
+            latest_subdir_number = max(latest_subdir_number, number)
+        except ValueError:
+            pass
+
+# Increment the subdirectory number
+latest_subdir_number += 1
+subdir_name = f"running logs {latest_subdir_number}"
+
+# Create the full path for the new log directory and log file
+log_subdir_path = os.path.join(log_dir, subdir_name)
+filepath = os.path.join(log_subdir_path, "logs.log")
+
+# Create the necessary directories
+os.makedirs(log_subdir_path, exist_ok=True)
+
+logging.basicConfig(
+    format=logging_str,
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler(filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
